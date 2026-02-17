310384b742044d79cddf41206cd88d7bc0d7179c
bhagyabanghadai <bhagyaban24523@gmail.com>
2026-02-17 16:00:00 -0700

    chore(cortex): export StateExtractor, MetaPolicy, OfflineTrainer, PromotionGate

--- celr/cortex/__init__.py
+++ celr/cortex/__init__.py
@@ -2,5 +2,10 @@ from .state import StateExtractor
 from .policy import MetaPolicy, CortexAction
 from .trainer import OfflineTrainer
 from .gatekeeper import PromotionGate
+from .council import HiveMindCouncil, CouncilDebate, Verdict, get_council
 
-__all__ = ["StateExtractor", "MetaPolicy", "CortexAction", "OfflineTrainer", "PromotionGate"]
+__all__ = [
+    "StateExtractor", "MetaPolicy", "CortexAction",
+    "OfflineTrainer", "PromotionGate",
+    "HiveMindCouncil", "CouncilDebate", "Verdict", "get_council",
+]
